1d5914e9b5c38b128855beaf7baadf210155d834
Asmer Bracho <asmer.alef@gmail.com>
2019-04-23 15:27:14 +0100

    Added First Version of Pie

--- app/src/main/java/ecoext/com/ecoext/HomeFragment.java
+++ app/src/main/java/ecoext/com/ecoext/HomeFragment.java
@@ -1,6 +1,7 @@
 package ecoext.com.ecoext;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +11,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 
@@ -36,6 +48,10 @@ public class HomeFragment extends Fragment {
         homePurseAdapter = new HomePurseAdapter(this.getContext(), purses);
         initReciclerView(view);
 
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        initGraph(graph);
+        pieChart(view);
+
         return view;
     }
 
@@ -44,5 +60,78 @@ public class HomeFragment extends Fragment {
         LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
         listOfPurses.setLayoutManager(layoutManager);
         listOfPurses.setAdapter(homePurseAdapter);
+    }
+
+    // Graph Function
+    public void initGraph(GraphView graph) {
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, -2),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        series.setSpacing(30);
+        series.setAnimated(true);
+        graph.addSeries(series);
+        series.setColor(Color.GREEN);
+
+        BarGraphSeries<DataPoint> series2 = new BarGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, -5),
+                new DataPoint(1, 3),
+                new DataPoint(2, 4),
+                new DataPoint(3, 4),
+                new DataPoint(4, 1)
+        });
+        series2.setColor(Color.RED);
+        series2.setSpacing(30);
+        series2.setAnimated(true);
+        graph.addSeries(series2);
+
+//        graph.getViewport().setXAxisBoundsManual(true);
+//        graph.getViewport().setMinX(-2);
+//        graph.getViewport().setMaxX(6);
+
+        // legend
+        series.setTitle("Income");
+        series2.setTitle("Expense");
+        graph.getLegendRenderer().setVisible(true);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+    }
+
+
+    public void pieChart(View view) {
+        PieChart pieChart = (PieChart) view.findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+
+
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        ArrayList<Entry> yvalues = new ArrayList<Entry>();
+        yvalues.add(new Entry(8f, 0));
+        yvalues.add(new Entry(15f, 1));
+        yvalues.add(new Entry(12f, 2));
+        yvalues.add(new Entry(25f, 3));
+        yvalues.add(new Entry(23f, 4));
+        yvalues.add(new Entry(17f, 5));
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "Election Results");
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        xVals.add("January");
+        xVals.add("February");
+        xVals.add("March");
+        xVals.add("April");
+        xVals.add("May");
+        xVals.add("June");
+
+        PieData data = new PieData(xVals, dataSet);
+
+        data.setValueFormatter(new PercentFormatter());
+
+        // setData
+        pieChart.setData(data);
     }
 }
